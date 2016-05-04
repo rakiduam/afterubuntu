df2311d770089241f890130082fa8281c1685e51
rakiduam <spartayotros@gmail.com>
2016-05-04 19:58:26 -0300

    orden

--- basico.sh
+++ basico.sh
@@ -4,32 +4,28 @@ echo "INSTALACION ESENCIAL DESPUES DE INSTALAR UBUNTU AGAIN"
 sudo apt update && sudo apt upgrade
 echo "COMPRIMIDOS"
 sudo apt install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract #file-roller
-
 echo "RESTRICTED EXTRAS"
 sudo apt install -y ubuntu-restricted-extras
-
+echo "PLAYONLINUX"
+sudo apt install -y playonlinux
 echo "TWEAK"
 sudo apt install -y unity-tweak-tool
 sudo apt install -y compizconfig-settings-manager
 sudo apt install -y gnome-tweak
-
 echo "bleachbit"
 sudo apt install -y bleachbit
-
 echo "indicator-cpufreq"
 sudo apt install -y indicator-cpufreq
-
 echo "Laptop-Tweaks"
 sudo add-apt-repository -y ppa:linrunner/tlp
 sudo apt update
 sudo apt install tlp tlp-rdw
 sudo tlp start
-
 echo "caffeine"
 sudo add-apt-repository -y ppa:caffeine-developers/ppa
 sudo apt install -y caffeine
 
-echo "##### APLICACIONES ######"
+echo "##### OTRAS APLICACIONES ######"
 # echo "MENDELEY"
 # wget https://www.mendeley.com/repositories/ubuntu/stable/amd64/mendeleydesktop-latest
 # sudo dpkg -i mendeleydesktop*.deb
