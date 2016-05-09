eb476c262cec6556144d1a4013ae98d71d88b875
rakiduam <spartayotros@gmail.com>
2016-05-09 16:57:56 -0300

    update

--- personalizacion.sh
+++ personalizacion.sh
@@ -6,24 +6,24 @@ echo "##### PERSONALIZACION ######"
 # sudo apt install variety
 
 echo "CONKY"
-sudo add-apt-repository -y ppa:teejee2008/ppa
-sudo apt-get update
-sudo apt-get install -y conky-manager
+#sudo add-apt-repository -y ppa:teejee2008/ppa
+#sudo apt-get update
+#sudo apt-get install -y conky-manager
 
 echo "TEMAS e ICONOS"
 ## en cuanto a iconos tal vez sea mejor descargar los paquetes
 ## e instalar uno a uno, ya que tienen dependencias con otros paquetes
 ## evitaria ver millones en unity tweak
-# #MOKA
-# sudo add-apt-repository ppa:moka/stable
-# sudo apt update
-# sudo apt install moka-icon-theme
-# #NUMIX
-# sudo add-apt-repository ppa:numix/ppa
-# sudo apt update
-# sudo apt install numix-icon-theme numix-icon-theme-circle
-#ELEMENTARY
-# sudo add-apt-repository ppa:elementary-os/daily
-# sudo apt update
-# sudo apt install elementary-icon-theme
+echo "MOKA icon-theme"
+sudo add-apt-repository ppa:moka/stable
+sudo apt update
+sudo apt install faba-icon-theme moka-icon-theme
+echo "NUMIX icon-theme"
+sudo add-apt-repository ppa:numix/ppa
+sudo apt update
+sudo apt install numix-icon-theme numix-icon-theme-circle
+echo "ELEMENTARY icon-theme"
+sudo add-apt-repository ppa:elementary-os/daily
+sudo apt update
+sudo apt install elementary-icon-theme
 #sudo apt remove elementary-icon-theme
